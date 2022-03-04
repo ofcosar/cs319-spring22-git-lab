9034868535e1da7d4c04b7df68fc98275dfbf8e6
omer faruk cosar <ofcosar1@gmail.com>
2022-03-04 10:46:18 +0300

    add multiplication feature

--- NewCalculator.java
+++ NewCalculator.java
@@ -1,27 +1,32 @@
 public class NewCalculator {
     private double lastResult;
 
-    public NewCalculator(){
+    public NewCalculator() {
         this.lastResult = 0;
     }
 
-    public double getLastResult(){
+    public double getLastResult() {
         return this.lastResult;
     }
 
-    public double Add(double x, double y){
+    public double Add(double x, double y) {
         this.lastResult = x + y;
         return this.lastResult;
     }
 
-    public double Subtract (double x, double y){
+    public double Subtract(double x, double y) {
         this.lastResult = x - y;
         return this.lastResult;
     }
 
-    public double Divide(double dividend, double divisor){
+    public double Divide(double dividend, double divisor) {
         this.lastResult = dividend % divisor;
         return this.lastResult;
     }
-    
+
+    public double Multiply(double x, double y) {
+        this.lastResult = x * y;
+        return this.lastResult;
+    }
+
 }
